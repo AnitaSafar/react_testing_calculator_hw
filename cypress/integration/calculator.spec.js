9e180f50265f83c9d5ff27b753d05172e3f4a683
Anita Safar <ancsa0416@gmail.com>
2021-11-23 19:15:09 +0000

    operations update display test passed

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -6,5 +6,13 @@ describe("Calculator", () => {
   it('should have working number buttons', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
-  })
+  });
+
+  it('operations should update the display with the result', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number6').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '18')
+  });
 })
